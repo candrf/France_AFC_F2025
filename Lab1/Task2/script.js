f36598c5f60228a58c719db4fb4c02bacd2dba72
Andrew France <andrew.c.france.mil@army.mil>
2025-08-26 19:28:51 -0500

    Task 2 complete

---
+++ Lab1/Task2/script.js
@@ -0,0 +1,24 @@
+// script.js
+// Name: Andrew France
+// Lab2 - Simple Interest Calculator
+
+// Step 1: Function to calculate interest, takes in principal, rate, and time
+function calculateSimpleInterest(principal, rate, time) {
+    // Do the calculation and return the simple interest
+    return (principal * rate * time) / 100;
+}
+
+// Step 2: Get the principal and store it in variable as a number
+const principal = Number(prompt("Enter the principal amount:"));
+
+// Step 3: Get the interest and store it in a variable as a number
+const rate = Number(prompt("Enter the rate of interest:"));
+
+// Step 4: Get the years and and store it in a variable as a number
+const time = Number(prompt("Enter the time in years:"));
+
+// Step 5: Calculate the interest with user defined variables and store results
+const interest = calculateSimpleInterest(principal, rate, time);
+
+// Step 6: Print the results to the console
+console.log("Your simple interest is:", interest);
