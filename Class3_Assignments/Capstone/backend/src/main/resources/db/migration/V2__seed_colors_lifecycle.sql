5b90f40a6c7fbb994c9b641327c376c13fd241dd
Andrew France <andrew.c.france.mil@army.mil>
2025-11-13 14:36:27 -0600

    seeded database with widgets

---
+++ Class3_Assignments/Capstone/backend/src/main/resources/db/migration/V2__seed_colors_lifecycle.sql
@@ -0,0 +1,10 @@
+INSERT INTO color (color_code, color_label, color_hex) VALUES
+('C001', 'Red', '#ff0000'),
+('C002', 'Green', '#00ff00'),
+('C003', 'Blue', '#0000ff'),
+('C004', 'Black', '#000000'),
+('C005', 'White', '#ffffff');
+
+INSERT INTO lifecycle (lifecycle_status) VALUES
+('active'),
+('inactive');
---
+++ Class3_Assignments/Capstone/backend/src/main/resources/db/migration/V3__seed_widgets.sql
@@ -0,0 +1,11 @@
+INSERT INTO widget (name, blurb, rating, quantity, price, slug, created_at, updated_at, image_url, color_id, lifecycle_id) VALUES
+('Headphones', 'The best noise canceling headphones', 3, 50, 20.95, 'headphones', NOW(), NOW(), 'http://localhost:8080/static/headphones.png', 1, 1),
+('Best Paperclip', 'The only paperclip you need', 1, 500, .99, 'best-paperclip', NOW(), NOW(), 'http://localhost:8080/static/bestpaperclip.png', 2, 1),
+('Alarm Clock', 'It wakes you up', 2, 130, 10.95, 'alarm-clock', NOW(), NOW(), 'http://localhost:8080/static/clock.png', 3, 1),
+('Universal Controller', 'It can control anything', 4, 1, 120000.95, 'universal-controller', NOW(), NOW(), 'http://localhost:8080/static/controller.png', 4, 1),
+('Magic Wand', 'It has the magic touch', 1, 500, .01, 'magic-wand', NOW(), NOW(), 'http://localhost:8080/static/magicwand.png', 5, 1),
+('Magnet', 'Very powerful magnet', 3, 44, 12.95, 'magnet', NOW(), NOW(), 'http://localhost:8080/static/magnet.png', 1, 1),
+('Mystery Potion', 'Who knows what it does', 5, 3, 149.95, 'mystery-potion', NOW(), NOW(), 'http://localhost:8080/static/potion.png', 2, 1),
+('Two-Way Radio', 'Talk to your friends', 2, 10, 50, 'two-way-radio', NOW(), NOW(), 'http://localhost:8080/static/radio.png', 3, 1),
+('Super Chip', 'It has the most compute power', 1, -10, 1000.99, 'super-chip', NOW(), NOW(), 'http://localhost:8080/static/superchip.png', 4, 2),
+('Ultra Wrench', 'The strongest wrench', 3, -3, 199.99, 'ultra-wrench', NOW(), NOW(), 'http://localhost:8080/static/ultrawrench.png', 5, 2);
